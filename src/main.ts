45b48beba03b56f5ec28fb6b7b6609e1559d39df
hokamsingh <singhhokam704@gmail.com>
2025-12-29 22:59:26 +0530

    chore: add global process error handlers

--- src/main.ts
+++ src/main.ts
@@ -26,4 +26,15 @@ async function bootstrap() {
 
   await app.listen(port);
 }
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
 bootstrap();
