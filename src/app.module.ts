34220f793721f21b3ed16ead37c0d11d0b5af6de
hokamsingh <singhhokam704@gmail.com>
2025-12-29 22:30:57 +0530

    feat: add health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
   imports: [DatabaseModule, SessionsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule { }
